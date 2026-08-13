ffb0625f3a53c940df7384df317bec1f0a0fa767
wkwtunisia <wkwtunisia@gmail.com>
2026-08-13 15:46:40 +0000

    Version stable finale : toutes les fonctionnalités intégrées

--- next.config.js
+++ next.config.js
@@ -4,13 +4,7 @@ const nextConfig = {
     domains: ['images.unsplash.com', 'firebasestorage.googleapis.com', 'lh3.googleusercontent.com'],
   },
   reactStrictMode: true,
-  // Désactiver les fonctionnalités expérimentales
-  experimental: {
-    serverActions: true,
-  },
-  // Configuration pour Cloudflare
-  output: 'standalone',
-  swcMinify: false, // Désactiver swcMinify pour Cloudflare
+  swcMinify: true,
 };
 
 module.exports = nextConfig;
